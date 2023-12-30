147b120b0cbe8cf89e78926e61deef050ad69afc
OFabuloso <johrod11@gmail.com>
2023-12-29 21:57:01 -0300

    concluido aula 1

--- hello.py
+++ hello.py
@@ -1,39 +1,39 @@
-#!/usr/bin/env
-"""
-Hello World Multi Language.
-the LAng in use the ambient for developer "env"
-the message is usual env language
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-usage:
-have a LANG var config sucesffuly in your env
-EX:
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
-Execution:
+
+Execução:
 
     python3 hello.py
-    or
+    ou
     ./hello.py
 """
 __version__ = "0.0.1"
-__author__ = "OFabuloso"
- 
-import os 
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
 
 
-# tem o shebang aprender mais sobre
-
-current_language = os.getenv("LANG")[:5]
-msg = "Hello, World"
+# Aqui aplicamos a condicional
 
 if current_language == "pt_BR":
-    msg = "E ae Zé Bunda"
+	msg = "Olá, Mundo!"
 elif current_language == "it_IT":
-    msg = "la mafia del pizza"
-else:
-    msg = "E deu ruim"
-
+	msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
 
 print(msg)
-
-
